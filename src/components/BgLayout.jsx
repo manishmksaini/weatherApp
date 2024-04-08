941ac123853f241e7df333722e9a48e71f530e43
manish <mksaini1046@gmail.com>
2024-04-08 16:36:05 +0530

    first commit

---
+++ src/components/BgLayout.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import { useStateContext } from "../Context";
+
+// import Clear from "../assets/images/Clear.jpg";
+// import Fog from "../assets/images/fog.png";
+// import Cloudy from "../assets/images/Cloudy.jpg";
+// import Rainy from "../assets/images/Rainy.jpg";
+// import Snow from "../assets/images/snow.jpg";
+// import Stormy from "../assets/images/Stormy.jpg";
+// import Sunny from "../assets/images/Sunny.jpg";
+
+import Clear from '../assets/videos/clear.mp4'
+import Fog from '../assets/videos/fog.mp4'
+import Cloudy from '../assets/videos/cloudy.mp4'
+import Rainy from '../assets/videos/Rainy.mp4'
+import Snow from '../assets/videos/snow.mp4'
+import Stormy from '../assets/videos/storm.mp4'
+import Sunny from '../assets/videos/sunny.mp4'
+
+const BgLayout = () => {
+  const { weather } = useStateContext();
+  const [image, setImage] = useState(Clear);
+
+  useEffect(() => {
+    if (weather.conditions) {
+      let imageString = weather.conditions;
+
+      if (imageString.toLowerCase().includes("clear")) {
+        setImage(Clear);
+      } 
+      else if (imageString.toLowerCase().includes("cloud")) {
+        setImage(Cloudy);
+      } 
+      else if (
+        imageString.toLowerCase().includes("rain") ||
+        imageString.toLowerCase().includes("shower")
+      ) {
+        setImage(Rainy);
+      } 
+      else if (imageString.toLowerCase().includes("snow")) {
+        setImage(Snow);
+      } 
+      else if (imageString.toLowerCase().includes("fog")) {
+        setImage(Fog);
+      } 
+      else if (
+        imageString.toLowerCase().includes("thunder") ||
+        imageString.toLowerCase().includes("storm")
+      ) {
+        setImage(Stormy);
+      }
+    }
+  }, [weather]);
+
+  return (
+    <video
+      src={image}
+      alt="weather_image"
+      width="100%"
+      className="  fixed left-0 top-0 -z-[10]" autoPlay loop muted
+    >
+
+    </video>
+    
+    // <img
+    //   src={image}
+    //   alt="weather_image"
+    //   className="h-screen w-full fixed left-0 top-0 -z-[10]"
+    // />
+    
+  );
+};
+
+export default BgLayout;
